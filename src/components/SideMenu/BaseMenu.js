8221c3b5866411573b03fcb06e1b985624b5d5df
JerryMissTom <582486638@qq.com>
2019-04-29 16:06:14 +0800

    添加Menu自动打开的功能

--- src/components/SideMenu/BaseMenu.js
+++ src/components/SideMenu/BaseMenu.js
@@ -5,6 +5,25 @@ import { FormattedMessage } from 'react-intl';
 const SubMenu = Menu.SubMenu;
 class BaseMenu extends PureComponent {
 
+
+    constructor(props) {
+        super(props)
+        const { routes, location: { pathname } } = this.props;
+        this.state = {
+            openKeys: this.getOpenKeys(routes, pathname)
+        }
+    }
+
+    // 刷新或者切换页面，被选中MenuItem的SubItem是处于打开状态
+    componentWillReceiveProps(nextProps) {
+        const { routes, location: { pathname } } = this.props;
+        if (pathname !== nextProps.location.pathname) {
+            this.setState({
+                openKeys: this.getOpenKeys(routes, nextProps.location.pathname)
+            });
+        }
+    }
+
     /**
      * 获得菜单子节点
      * @memberof SiderMenu
@@ -47,14 +66,44 @@ class BaseMenu extends PureComponent {
         </Menu.Item>;
     }
 
+    onOpenChange = (openKeys) => {
+        this.setState({
+            openKeys: openKeys
+        })
+    }
+
+    // 获取被选中 MenuItem 的所有父级 SubItem
+    getOpenKeys = (routes, currentpath) => {
+
+        let stack = [];
+        let going = true;
+
+        let walker = (array, path) => {
+            array.forEach(item => {
+                if (!going) return;
+                stack.push(item['path']);
+                if (item['path'] === path) {
+                    going = false;
+                } else if (item['children']) {
+                    walker(item['children'], path);
+                } else {
+                    stack.pop();
+                }
+            });
+            if (going) stack.pop();
+        }
+        walker(routes, currentpath);
+        return stack;
+    }
 
     render() {
         const { handleClick, routes, location: { pathname } } = this.props;
-
         return (
             <Menu
                 theme='dark'
                 onClick={handleClick}
+                openKeys={this.state.openKeys}
+                onOpenChange={this.onOpenChange}
                 selectedKeys={[pathname]}
                 mode="inline"
             >
